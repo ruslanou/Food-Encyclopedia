4a9ba6b009210fa41ea2c7501a4b472ce167b867
Ahmad Ruslan <aruslan8395@gmail.com>
2020-05-27 19:25:24 +0700

    Initial Commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:food_encylopedia/models/category.dart';
+
+const dummy_categories = [
+  Category(
+    id: '1',
+    title: 'Italian',
+    color: Colors.purple
+  ),
+  Category(
+    id: '2',
+    title: 'Quick & Easy',
+    color: Colors.red
+  ),
+  Category(
+    id: '3',
+    title: 'Hamburgers',
+    color: Colors.orange
+  ),
+  Category(
+    id: '4',
+    title: 'German',
+    color: Colors.amber
+  ),
+  Category(
+    id: '5',
+    title: 'Light & Lovely',
+    color: Colors.blue
+  ),
+  Category(
+    id: '6',
+    title: 'Exotic',
+    color: Colors.green
+  ),
+  Category(
+    id: '7',
+    title: 'Breakfast',
+    color: Colors.lightBlue
+  ),
+  Category(
+    id: '8',
+    title: 'Asian',
+    color: Colors.lightGreen
+  ),
+  Category(
+    id: '9',
+    title: 'French',
+    color: Colors.pink
+  ),
+  Category(
+    id: '10',
+    title: 'Summer',
+    color: Colors.teal
+  ),
+];
